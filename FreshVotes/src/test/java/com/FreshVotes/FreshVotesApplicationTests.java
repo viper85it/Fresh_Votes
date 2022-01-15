b3e65707faa7b6e7e3a7a59937fd51950069f3fa
viper <viper@HP-VP>
2022-01-15 00:11:45 -0600

    First Commit

---
+++ FreshVotes/src/test/java/com/FreshVotes/FreshVotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FreshVotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
